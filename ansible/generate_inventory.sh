d79cca6513c8d080c0864bb537f654e31c362755
Zied-BenBahri <ziedbenbahri@gmail.com>
2025-12-02 14:38:12 +0100

    Refactor Ansible inventory generation and update Jenkins pipeline for improved deployment process

--- ansible/generate_inventory.sh
+++ ansible/generate_inventory.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 IP=$(terraform output -raw vm_public_ip)
+PASSWORD=$(grep admin_password terraform.tfvars | cut -d'"' -f2)
 
 mkdir -p ansible
 
-cat > ansible/inventory.ini << EOF
+cat > ansible/inventory.ini << EOINV
 [web]
-$IP ansible_user=azureuser ansible_ssh_pass=$(grep admin_password terraform.tfvars | cut -d'"' -f2)
+$IP ansible_user=azureuser ansible_ssh_pass=$PASSWORD
 
 [web:vars]
 ansible_python_interpreter=/usr/bin/python3
+EOINV
 EOF
